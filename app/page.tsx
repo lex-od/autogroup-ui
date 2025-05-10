0b56c59a473728247eadc67398e27e82d4a9b704
Aleksey Yesin <esinru@gmail.com>
2025-05-10 17:38:25 +0300

    Adds basic elements

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,19 @@
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 
 export default function Home() {
   return (
-    <div className="p-8">
-      <p className="mb-2">hello</p>
-      <Button className="mr-2">Click me</Button>
-      <Button variant="secondary">Secondary</Button>
+    <div className="p-4 grid gap-4">
+      <h1>Basic elements</h1>
+
+      <Input className="max-w-xs" />
+
+      <div className="grid grid-flow-col gap-4 auto-cols-max">
+        <Button className="min-w-20">Ok</Button>
+        <Button className="min-w-20" variant="secondary">
+          Cancel
+        </Button>
+      </div>
     </div>
   );
 }
