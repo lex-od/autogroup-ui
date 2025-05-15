f890d825ff952557dafa7fe2bebf781d74805460
Aleksey Yesin <esinru@gmail.com>
2025-05-15 17:48:18 +0300

    Adds react query

---
+++ components/root/QueryProvider.tsx
@@ -0,0 +1,17 @@
+'use client';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { FC, PropsWithChildren } from 'react';
+
+const queryClient = new QueryClient();
+
+const QueryProvider: FC<PropsWithChildren> = ({ children }) => {
+  return (
+    <QueryClientProvider client={queryClient}>
+      {children}
+      <ReactQueryDevtools />
+    </QueryClientProvider>
+  );
+};
+
+export default QueryProvider;
